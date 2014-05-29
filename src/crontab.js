d33db524630dd632129cdff6605239089cc19846
yss <yss.nelson@gmail.com>
2014-05-29 18:22:40 +0800

    complete the basic of crontab.js

--- src/crontab.js
+++ src/crontab.js
@@ -3,7 +3,7 @@
 /**
  * for manage setTimeout and setInterval
  * @param {Number} [interval] default: 400ms
- * @param {Number} [error] default: 100ms
+ * @param {Number} [error] default: 100ms error < interval
  */
 function Crontab(interval, error) {
     if (!(this instanceof Crontab)) {
@@ -11,19 +11,31 @@ function Crontab(interval, error) {
     }
     this._stack = {};
     this._id = 0;
-    this._error = error;
+    this._error = error || 100;
     this.setInterval(interval);
     return this;
 }
 
 Crontab.prototype = {
+    /**
+     * register a setInterval
+     * @param {Number} time
+     * @param {Function} fn
+     * @return {Number}
+     */
     on: function(time, fn) {
-        this._stack[this._id++] = {
+        this._stack[++this._id] = {
             time: time,
+            last: new Date().getTime(),
             fn: fn
         };
+        return this._id;
     },
 
+    /**
+     * remove setInterval, setTimeout
+     * @param {Number} id
+     */
     off: function(id) {
         if (id) {
             delete this._stack[id];
@@ -32,6 +44,12 @@ Crontab.prototype = {
         }
     },
 
+    /**
+     * register once. equal to setTimeout
+     * @param {Number} time
+     * @param {Function} fn
+     * @return {Number}
+     */
     one: function(time, fn) {
         var _this = this;
         var id =_this.on(time, function() {
@@ -42,18 +60,32 @@ Crontab.prototype = {
         return id;
     },
 
+    /**
+     * start the crontab.
+     */
     start: function() {
         var _this = this;
-        _this._startTime = new Date().getTime();
+        // for repeat start
+        if (this._timer) {
+            return;
+        }
         _this._timer = win.setInterval(function() {
             _this._run();
-        }, _this.interval);
+        }, _this._interval);
     },
 
+    /**
+     * stop the crontab.
+     */
     stop: function() {
-        win.clearInterval(_this._timer);
+        this._timer && win.clearInterval(this._timer);
+        this._timer = null;
     },
 
+    /**
+     * set the interval config
+     * @param {Number} [interval] default: 400ms
+     */
     setInterval: function(interval) {
         interval = interval || 400;
         if (this._interval !== interval) {
@@ -64,13 +96,27 @@ Crontab.prototype = {
     },
 
     _run: function() {
-        var stack = this.stack,
-            time = new Date().getTime() - this._startTime + this._error,
-            key;
+        var stack = this._stack,
+            currTime = new Date().getTime() + this._error,
+            key,
+            runTime,
+            runFn = [];
         for(key in stack) {
-            if (stack[key].time < time) {
-                stack[key].fn.call(win);
+            runTime = stack[key].last + stack[key].time;
+            if (currTime > runTime) {
+                runFn.push({
+                    t: runTime,
+                    k: key
+                });
             }
+        }
+
+        runFn.sort(function(a, b) {
+            return a.t < b.t;
+        });
+
+        while ((key = runFn.pop())) {
+            stack[key.k].fn.call(win);
         }
     }
 }
