fe5b41f46d92adba5fc0f0367de376982d03e365
yansong <yansong@fenbi.com>
2017-06-11 16:51:43 +0800

    MOD: use setTimeout instead of setInterval, and use Map instead of Object for store running queue

--- src/crontab.js
+++ src/crontab.js
@@ -2,27 +2,22 @@
  * Author: yss
  */
 
-(function(global) {
-
-var instance = null;
+let instance = null;
 
 /**
  * for manage setTimeout and setInterval
  * @param {Number} [interval] default: 400ms
  * @param {Number} [tolerance] default: 100ms tolerance < interval
  */
-function Crontab(interval, tolerance) {
-    if (!(this instanceof Crontab)) {
-        return new Crontab(interval, tolerance);
-    }
-    this._stack = {};
-    this._id = 0;
-    this._tolerance = tolerance || 100;
-    this.setIntervalTime(interval);
-    return this;
-}
+class Crontab {
 
-Crontab.prototype = {
+    constructor (interval, tolerance) {
+        this._store = new Map();
+        this._id = 0;
+        this._tolerance = tolerance || 100;
+        this.setIntervalTime(interval);
+    }
+
     /**
      * register a setInterval
      * @param {Function} fn
@@ -30,29 +25,28 @@ Crontab.prototype = {
      * @param {Any} [context]
      * @return {Number}
      */
-    on: function(fn, time, context) {
-        var obj = {
-            time: time,
-            last: this._getTime(),
-            fn: fn
-        };
+    on (fn, time, context) {
+        this._store.set(++this._id, {
+            last: Date.now(),
+            fn,
+            time,
+            context
+        });
 
-        context && (obj.context = context);
-        this._stack[++this._id] = obj;
         return this._id;
-    },
+    }
 
     /**
      * remove setInterval, setTimeout
      * @param {Number} id
      */
-    off: function(id) {
+    off (id) {
         if (id) {
-            delete this._stack[id];
+            this._store.delete(id);
         } else {
-            this._stack = {};
+            this._store.clear();
         }
-    },
+    }
 
     /**
      * register once. equal to setTimeout
@@ -61,147 +55,134 @@ Crontab.prototype = {
      * @param {Any} [context]
      * @return {Number}
      */
-    one: function(fn, time, context) {
-        var _this = this;
-        var id =_this.on(function() {
+    one (fn, time, context) {
+        let _this = this;
+        const id = _this.on(function() {
             fn && fn.call(this);
+
             _this.off(id);
         }, time, context);
 
         return id;
-    },
+    }
 
     /**
      * alias to `on`
      */
-    setInterval: function() {
+    setInterval () {
         this.on.apply(this, arguments);
-    },
+    }
 
     /**
      * alias to `off(id)`
      * @param {Number} id
      */
-    clearInterval: function(id) {
+    clearInterval (id) {
         id && this.off(id);
-    },
+    }
 
     /**
      * alias to `one`
      */
-    setTimeout: function() {
+    setTimeout () {
         this.one.apply(this, arguments);
-    },
+    }
 
     /**
      * alias to `off(id)`
      * @param {Number} id
      */
-    clearTimeout: function(id) {
+    clearTimeout (id) {
         id && this.off(id);
-    },
+    }
 
     /**
      * start the crontab.
      */
-    start: function() {
-        var _this = this;
+    start () {
+        let _this = this;
         // for repeat start
-        if (this._timer) {
+        if (_this._timer) {
             return;
         }
-        _this._timer = setInterval(function() {
+        _this._timer = setTimeout(function() {
             _this._run();
+            _this._timer = null;
+            _this.start();
         }, _this._interval);
-    },
+    }
 
     /**
      * stop the crontab.
      */
-    stop: function() {
-        this._timer && clearInterval(this._timer);
-        this._timer = null;
-    },
+    stop () {
+        if (this._timer) {
+            clearTimeout(this._timer);
+            this._timer = null;
+        }
+    }
 
     /**
      * set the config of interval
      * @param {Number} [interval] default: 400ms
      */
-    setIntervalTime: function(interval) {
+    setIntervalTime (interval) {
         interval = interval || 400;
         if (this._interval !== interval) {
             this._interval = interval;
             this.stop();
             this.start();
         }
-    },
+    }
 
     /**
      * set the config of tolerance
      * @param tolerance
      */
-    setToleranceTime: function(tolerance) {
+    setToleranceTime (tolerance) {
         if (tolerance && tolerance >= 0 && this._interval > tolerance) {
             this._tolerance = tolerance;
         }
-    },
+    }
 
-    _run: function() {
-        var stack = this._stack,
-            currTime = this._getTime() + this._tolerance,
-            key,
-            runTime,
-            runFn = [];
-        for(key in stack) {
-            runTime = stack[key].last + stack[key].time;
-            if (currTime > runTime) {
-                runFn.push({
-                    t: runTime,
-                    k: key
+    _run () {
+        let currentTime = Date.now() + this._tolerance;
+        let runStack = [];
+
+        for (let value of this._store.values()) {
+            let runTime = value.last + value.time;
+
+            if (currentTime > runTime) {
+                runStack.push({
+                    runTime,
+                    value
                 });
             }
         }
 
         // empty
-        if (!runFn.length) {
+        if (runStack.length === 0) {
             return;
         }
 
-        // sort by time desc
-        runFn.sort(function(a, b) {
-            return a.t < b.t;
-        });
+        currentTime -= this._tolerance;
 
-        currTime -= this._tolerance;
-        while ((key = runFn.pop())) {
-            stack[key.k].last = currTime;
-            stack[key.k].fn.call(stack[key.k].context || global);
-        }
-    },
+        // sort by time ASC and running one by one
+        runStack.sort((a, b) => a.runTime - b.runTime)
+            .forEach(item => {
+                let value = item.value;
+                value.last = currentTime;
+                value.fn.call(value.context || null);
+            });
+
+    }
 
     /**
-     * get the current Time, unit is ms
+     * for global invoke.
      */
-    _getTime: function() {
-        return Date.now ? Date.now() : +new Date();
+    static getInstance () {
+        return instance || new Crontab();
     }
-};
-
-/**
- * for global invoke.
- */
-Crontab.getInstance = function() {
-    return instance || Crontab();
-};
-
-// for node environment
-if ( typeof module === "object" && module && typeof module.exports === "object" ) {
-    module.exports = Crontab;
-} else if ( typeof define === "function" && (define.amd || define.cmd)) {
-    // for cmd, amd
-    define("crontab", [], function() { return Crontab; } );
-} else {
-    // for global environment. like window
-    global.Crontab = Crontab;
 }
-})(this);
+
+module.exports = Crontab;
